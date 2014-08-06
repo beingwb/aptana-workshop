74abfcc825cce0ba28dbb0c8e95aa4242569ed1d
beingwb <datougan@qq.com>
2014-08-06 14:57:12 +0800

    增加了微信分享
    调整了在手机上的显示无法缩放
    响应触摸事件
    调整了css

--- chengxu.js
+++ chengxu.js
@@ -12,9 +12,9 @@ function paixu(){
 function chushihua(){
     document.write("<div id=\"qipan\">");
     for (var i = 0; i < 25; i++) {
-        // document.write("<span id=s" + i + " ontouchstart=\"down(" + i + ")\" ontouchend=\"up(" + i + ")\">" + x[i] + "</span>");
+         document.write("<span id=s" + i + " ontouchstart=\"down(" + i + ")\" ontouchend=\"up(" + i + ")\"></span>");
         //  document.write("<span id=s" + i + " onmousedown=\"down(" + i + ")\" onmouseup=\"up(" + i + ")\"></span>");
-        document.write("<span id=s" + i + " ontouchstart=\"down(" + i + ")\" ontouchend=\"up(" + i + ")\" onmousedown=\"down(" + i + ")\" onmouseup=\"up(" + i + ")\" ></span>");
+      //  document.write("<span id=s" + i + " ontouchstart=\"down(" + i + ")\" ontouchend=\"up(" + i + ")\" onmousedown=\"down(" + i + ")\" onmouseup=\"up(" + i + ")\" ></span>");
     }
     document.write("</div>");
     tianchong();
@@ -39,9 +39,10 @@ function up(x){
     document.getElementById('s' + x).style.background = "gray";
     if (dian == 26) {
         clearTimeout(t);
+        chengji = document.getElementById('miaobiao').innerHTML
         //document.getElementById("kongzhi").innerHTML = "重新开始"
-		alert(document.getElementById('miaobiao').innerHTML);
-		weixinfenxiang();
+        alert(chengji);
+        weixinfenxiang();
     }
     return false;
 };
@@ -81,15 +82,67 @@ function kaishiwan(){
 
 
 function weixinfenxiang(){
-    if (typeof WeixinJSBridge == "undefined") {
-        alert("请先添加好友再通过微信分享");
-    }
-    else {
-        WeixinJSBridge.invoke('shareTimeline', {
-            "title": "我在注意力测试游戏中的最好成绩为xxx秒",
-            "link": "地址链接",
-            "desc": "一款注意力测试的小游戏，小心上瘾哦",
-            "img_url": "logo.jpg"
-        });
-    }
+
+
+    // 需要分享的内容，请放到ready里
+    WeixinApi.ready(function(Api){
+    
+    
+        // 微信分享的数据
+        var wxData = {
+            "appId": "", // 服务号可以填写appId
+            "imgUrl": 'http://10.0.51.127/logo.jpg',
+            "link": 'http://10.0.51.127/',
+            "desc": chengji+'秒，我在注意力测试小游戏中的最好成绩。你也来试试吧：）',
+            "title": "注意力训练小游戏测试"
+        };
+        
+        
+        // 分享的回调
+        var wxCallbacks = {
+            // 分享操作开始之前
+            ready: function(){
+                // 你可以在这里对分享的数据进行重组
+                //  alert("准备分享");
+            },
+            // 分享被用户自动取消
+            cancel: function(resp){
+                // 你可以在你的页面上给用户一个小Tip，为什么要取消呢？
+                //   alert("分享被取消");
+            },
+            // 分享失败了
+            fail: function(resp){
+                // 分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试？
+                //    alert("分享失败");
+            },
+            // 分享成功
+            confirm: function(resp){
+                // 分享成功了，我们是不是可以做一些分享统计呢？
+                //window.location.href='http://192.168.1.128:8080/wwyj/test.html';
+                //  alert("分享成功");
+            },
+            // 整个分享过程结束
+            all: function(resp){
+                // 如果你做的是一个鼓励用户进行分享的产品，在这里是不是可以给用户一些反馈了？
+                //   alert("分享结束");
+            }
+        };
+        
+        
+        // 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
+        Api.shareToFriend(wxData, wxCallbacks);
+        
+        
+        // 点击分享到朋友圈，会执行下面这个代码
+        Api.shareToTimeline(wxData, wxCallbacks);
+        
+        
+        // 点击分享到腾讯微博，会执行下面这个代码
+        Api.shareToWeibo(wxData, wxCallbacks);
+        
+        
+    });
+    
+    
+    
 }
